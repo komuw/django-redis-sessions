ef837f9a5609f6c40ea586780295ddd285565f4b
johnmilimo <johnwanja@gmail.com>
2018-07-06 10:21:44 +0300

    simplified logic

--- redis_sessions/store_selector.py
+++ redis_sessions/store_selector.py
@@ -1,11 +1,5 @@
-from enum import Enum
 from redis_sessions.session import SessionStoreAlt
 from redis_sessions.session import SessionStore
-
-
-class States(Enum):
-    NO_MIGRATION = "NO_MIGRATION"
-    MIGRATING = "MIGRATING"
 
 
 class SessionStoreHandler:
@@ -13,95 +7,49 @@ class SessionStoreHandler:
     def __init__(self):
         pass
 
-    '''
-    > The values of [current_store], [alternative_store], and [current_state] 
-    are dynamic.
-    > They are set and updated by the SessionStoreHandler based on different scenarios.
-    > They need to be shared across all processes/requests. 
-    > For persistence, they need to be stored somewhere where they can be retrieved later.
-        A database will help.
-    '''
-
-    current_store = "session_store"
-    alternative_store = "session_store_alt"
-    current_state = States.NO_MIGRATION
-
     @staticmethod
-    def get_store_object(store_key):
-        if store_key == "session_store_alt":
-            return SessionStoreAlt
-
+    def get_current_store():
+        # the default session store
         return SessionStore
 
     @staticmethod
-    def get_current_store():
-        store_key = SessionStoreHandler.current_store
-        return SessionStoreHandler.get_store_object(store_key)
-
-    @staticmethod
     def get_alternative_store():
-        store_key = SessionStoreHandler.alternative_store
-        return SessionStoreHandler.get_store_object(store_key)
+        # the alternative session store to migrate new sessions to
+        return SessionStoreAlt
 
     @staticmethod
     def check_session_existence(session_store, session_key):
         return session_store().exists(session_key)
 
-    @staticmethod
-    def complete_migration():
-
-        # swap value of [current] with value of [alternative]
-        '''
-        :return: nil
-        After swapping the values, the change needs to be stored persistently,
-        so it can be shared across requests/processes, for django-redis
-        to know which session store is in active usage.
-        '''
-        SessionStoreHandler.current_store, SessionStoreHandler.alternative_store = \
-            SessionStoreHandler.alternative_store, SessionStoreHandler.current_store
-
-        # Set current_state to NO_MIGRATION
-        SessionStoreHandler.record_current_state(States.NO_MIGRATION)
-
-    @staticmethod
-    def record_current_state(state):
-        SessionStoreHandler.current_state = state
-
 
 class StoreSelector(SessionStoreHandler):
 
-    def __init__(self, session_key, migration_mode):
+    def __init__(self, session_key, migration_mode, drop_original_store=False):
         SessionStoreHandler.__init__(self)
+
+        # customer session key
         self.session_key = session_key
+
+        # whether to migrate new sessions to the alternative store
         self.migration_mode = migration_mode
 
-    # this is the entrypoint to be called from USSD
-    def get_session_store(self):
-        """
-        We don't know whether request session is new or already
-        exists in current store or alternative store . We have to find out
-        """
-        session_store = self.get_current_store()
-        session_exists = self.check_session_existence(session_store,
-                                                      self.session_key)
-        if session_exists:
-            # return the current session store
-            return session_store
-        elif (not session_exists) and self.migration_mode:
-            self.record_current_state(States.MIGRATING)
-            # get and return the alternative session store
-            session_store = self.get_alternative_store()
-            return session_store
+        # whether to continue checking for active sessions on the previous
+        # session store after migrating new sessions to alternative store
+        self.drop_original_store = drop_original_store
 
-        elif (not session_exists) and (not self.migration_mode)\
-                and (self.current_state == States.MIGRATING):
+    def get(self):
 
-            # complete the migration by updating the current_state variable
-            self.complete_migration()
-            # get and return the current session store
+        if not self.drop_original_store:
             session_store = self.get_current_store()
-            return session_store
-
+            if not self.migration_mode:
+                return session_store
+            else:
+                session_exists = self.check_session_existence(session_store,
+                                                              self.session_key)
+                if session_exists:
+                    # return the current session store
+                    return session_store
+                else:
+                    return self.get_alternative_store()
         else:
-            # return the current session store
-            return session_store
+            return self.get_alternative_store()
